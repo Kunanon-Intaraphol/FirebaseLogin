221ef1edc9eea90de1522efe559edbbcad68ffd6
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2022-01-26 11:28:59 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -94,6 +94,7 @@ const Contact = () => {
     <form className="form" onSubmit={handleSubmit}>
     
       <h1>ตรวจสอบข่าวปลอม</h1>
+      <img src = {pictureUrl} style={{width:200, height:200 ,borderRadius:10}}/>
       <h2>สวัสดีคุณ : {displayName}</h2>
         <h2>โปรดระบุข่าว</h2>
 
