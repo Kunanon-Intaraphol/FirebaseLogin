a8a74395a2a32f118b2649516df83a5c0778ff05
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2022-01-30 14:06:54 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -95,7 +95,7 @@ const Contact = () => {
   return (
     <form className="form" onSubmit={handleSubmit}>
       <center>
-        <img src = {pictureUrl} style={{width:200, height:200 ,borderRadius:10,marginTop:20,marginBottom:20,marginTop:20}}/>
+        <img src = {pictureUrl} style={{width:200, height:200 ,borderRadius:10,marginTop:20,marginBottom:20,marginTop:40}}/>
         <h2>สวัสดีคุณ : {displayName}</h2>
       </center>
     
