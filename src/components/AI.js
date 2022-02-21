0921d7dfb7bd349c7ddf7d65435d7181e51b1a39
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2022-02-21 15:36:23 +0700

    ััััี่่
    ั

--- src/components/AI.js
+++ src/components/AI.js
@@ -54,7 +54,7 @@ const Read = () => {
     const Fetchdata = ()=>{
         // const Uid = userId
 
-        db.collection("contacts").document("U960c2d93e6a6e98e3fbf59ccf9be8a2e").get().then((querySnapshot) => {
+        db.collection("contacts").get().then((querySnapshot) => {
             console.log("incollection")
             // Loop through the data and store
             // it in array to display
