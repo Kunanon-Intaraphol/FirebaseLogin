cd52d5cab3b552b3ee9f59f550f8976b7a00a6dc
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2022-03-04 09:30:48 +0700

    dfgmdlkg

--- src/components/AI.js
+++ src/components/AI.js
@@ -1,71 +1,25 @@
 // Import Firestore database
 import {db} from "../firebase";
-import React, { useState , useEffect } from 'react';
+import React, { useState } from 'react';
 import './read.css';
-// import PercentageCircle from 'reactjs-percentage-circle';
 import { CircularProgressbar , buildStyles} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import ChangingProgressProvider from "./ChangingProgressProvider"; 
-// import liff from '@line/liff';
 
 
 const Read = () => {
     
-    // const { data } = this.props.Uid
-
-
-    // setTimeout(function() {
-    //     window.location.reload(false);
-    // }, 7000)
-    
     const [info , setInfo] = useState([]);
 
-    // const [userId, setUserId] = useState("");    
-    // const [displayName, setDisplayName] = useState("");
-    // const [idToken, setIdToken] = useState("");
-    // const [pictureUrl, setPictureUrl] = useState("");
-
-    // const initLine = () => {
-    //     liff.init({ liffId: '1656553430-qgAJV55b' }, () => {
-    //       if (liff.isLoggedIn()) {
-    //         runApp();
-    //       } else {
-    //         liff.login();
-    //       }
-    //     }, err => console.error(err));
-    //     }
-    // const runApp = () => {
-    //     const idToken = liff.getIDToken();
-    //     setIdToken(idToken);
-    //     liff.getProfile().then(profile => {
-    //       console.log(profile);
-    //       setDisplayName(profile.displayName);
-    //       setUserId(profile.userId);
-    //       setPictureUrl(profile.pictureUrl);
-    //     }).catch(err => console.error(err));
-    // }
-    //   useEffect(() => {
-    //     initLine();
-    // }, []);
-    
-    
-
-    // Start the fetch operation as soon as
-    // the page loads
     window.addEventListener('load', () => {
         Fetchdata();
         console.log("Fetchdata")
       });
   
-    // Fetch the required data using the get() method
-    const Fetchdata = ()=>{
 
-        // const Uid = userId
+    const Fetchdata = ()=>{
 
         db.collection("contacts").get().then((querySnapshot) => {
             console.log("incollection")
-            // Loop through the data and store
-            // it in array to display
             querySnapshot.forEach(element => {
                 var contacts = element.data();
                 setInfo(arr => [...arr , contacts]);
@@ -75,16 +29,25 @@ const Read = () => {
         })
     }
       
-    // Display the result on the page
     return (
         <div>
             <center>
             <h3>NewsProof</h3> 
-            {/* <img src = {pictureUrl} style={{width:200, height:200 ,borderRadius:10,marginTop:20,marginBottom:20}}/>
-            <h2>สวัสดีคุณ : {displayName}</h2>/
-            <h2>{userId}</h2>
-            <h2>{idToken}</h2> */}
-            
+
+            <button style={{    
+            width: "250px",
+            height: "60px",
+            color: "#ebedf1",
+            backgroundColor: '#3F89F3',
+            fontSize:'20px',
+            borderRadius:'10px',
+            position:"absolute",
+            top:"30%",
+            }}
+            onClick={() => window.location.reload(false)}
+            >
+            กดเพื่อรับผลคำตอบ
+            </button>
             </center>
             {
                 info.map((contacts) => (
@@ -106,11 +69,17 @@ const Read = () => {
 
 const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
 
-    // const data = [
-    //     { name: onlyST[0], gender: STonlyP[0]  },
-    //     { name:onlyST[1], gender: STonlyP[1] },
-    //     { name: onlyST[2], gender: STonlyP[2] },
-    // ]
+    const data = []
+    let lengthonlyST =  onlyST.length;
+    for(var i=0;i<lengthonlyST;i++){
+        console.log(i);
+        
+
+        data.push(
+            { name: onlyST[i], gender: STonlyP[i]  },
+        )
+    }
+
 
     console.log(News + " " + Sentence + " " + FAI + " " + TAI + " " + INTFAI+ " " + onlyST+ " " + STonlyP);
     if(Status == "read"){
@@ -137,28 +106,28 @@ const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
     </div>
 
     <h4 style={{marginTop:"30px"}}>มีความเสี่ยงที่จะเป็นข่าวปลอม  {"\n"}</h4> <h4><h8 style={{fontSize:"22px",color:"#e82a4d"}}>{FAI}%{"\n"}</h8></h4>
-    {/* <h4>มีความคล้ายที่จะเป็นข่าวจริง {"\n"}</h4> <h4><h9 style={{fontSize:"22px",color:"#18d93e"}}>{TAI}%{"\n"}</h9></h4> */}
 
     <p>
     <h4>เนื้อหาข่าว </h4> {News}
     </p>
 
-    {/* <div className="AppTable">
+            
+    <div className="AppTable">
                 <table>
                     <tr>
                         <th>ประโยคที่มีความคล้ายที่จะเป็นข่าวปลอม</th>
-                        <th style={{borderLeft:"4px solid #3F89F3"}}>เปอร์เซ็นต์</th>
+                        <th>%</th>
                     </tr>
                     {data.map((val, key) => {
                     return (
                         <tr key={key}>
                             <td>{val.gender}</td>
-                            <td style={{borderLeft:"4px solid #3F89F3"}}>{val.name}</td>
+                            <td>{val.name}</td>
                         </tr>
                     )
                     })}
                 </table>
-            </div>         */}
+            </div>
     
                 </div>
             </center>
@@ -170,6 +139,10 @@ const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
 
     else{
 
+        setTimeout(function() {
+            window.location.reload(false);
+        }, 3000)
+
         return(
            
         
@@ -178,18 +151,19 @@ const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
     <p style={{fontSize: '20px'}}>โปรดรอสักครู่ ...ระบบกำลังประมวลผล... กรุณากดปุ่มเพื่อรับคำตอบ</p>     
 
     <button style={{    
-        width: "250px",
-        height: "60px",
-        marginTop:'30px',
-        color: "#ebedf1",
-        backgroundColor: '#3F89F3',
-        fontSize:'20px',
-        borderRadius:'5px',
-        }}
-        onClick={() => window.location.reload(false)}
-        >
-        กดเพื่อรับผลคำตอบ
-    </button>
+            width: "250px",
+            height: "60px",
+            color: "#ebedf1",
+            backgroundColor: '#3F89F3',
+            fontSize:'20px',
+            borderRadius:'10px',
+            position:"absolute",
+            top:"30%",
+            }}
+            onClick={() => window.location.reload(false)}
+            >
+            กดเพื่อรับผลคำตอบ
+            </button>
                 </div>
             </center>
            
